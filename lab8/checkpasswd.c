47ffe7edea24b9cc73d67a271307ca34a24684cd
tabeebyeamin <tabeeb.yeamin@mail.utoronto.ca>
2020-03-12 13:40:35 -0400

    comments

--- lab8/checkpasswd.c
+++ lab8/checkpasswd.c
@@ -12,9 +12,13 @@ int main() {
     scanf("%s", userid);
     printf("Password:\n");
     scanf("%s", password);
+
+    // Create a file descriptor to use for the pipe
     int fd[2];
     int pid;
     
+    /* Pipe and fork error checks
+    */
     if(pipe(fd) == -1) {
         perror("pipe");
         exit(1);
@@ -22,38 +26,58 @@ int main() {
     if((pid = fork()) == -1) {
         perror("fork");
         exit(1);
-
-    // child executes the validate entry
-    } else if(pid == 0) {
-
-
+    } 
+    /*
+    child executes validate, closes the end of the pipe
+    it doesn't need.
+    */
+    else if(pid == 0) {
+        // receiving data from parent process
         close(fd[1]);
 
+        // stdin into the validate function
         dup2(fd[0], fileno(stdin));
 
+        // execlp means you need exact path 
         execlp("./validate", "./validate", NULL);
+
+        // note, the code after exec doesn't get called.
         perror("exec");
         exit(1);
 
-    // parent
+    /*
+    Parent writes to the other end of the pipe, closing
+    the child's end
+
+    It waits for the child to finish, closes its own end
+    after it's done
+    */
     } else {
         
         close(fd[0]);
 
+        // writing the data to its end, twice 
+        // (read validate to see why)
         write(fd[1], userid, 10);
         write(fd[1], password, 10);
-
+        
         close(fd[1]);
         int status;
         
         // parent waits for child to terminate exec
+        // wait error check
         if((wait(&status)) == -1) {
             perror("wait");
             exit(1);
 
 
         } else {
+            // if child exited
             if(WIFEXITED(status)) {
+                /*
+                Successful match if child exited with a status of 0
+                (check validate.c to see how this works)
+                */
                 if(WEXITSTATUS(status) == 0){
                     printf("Password verified\n");
                 } else {
